200455f7ec9dbf0881ac8e69002a88d0ae8b325a
jowu <2783613453@qq.com>
2019-08-27 21:21:55 +0800

    Initial commit

---
+++ mybatis/src/test/java/cn/jy/mybatis/test/TestJdbc.java
@@ -0,0 +1,14 @@
+package cn.jy.mybatis.test;
+
+import org.junit.Test;
+
+/**
+ * @author DengQiang.Wu
+ * @create 2019-08-23 13:00
+ */
+public class TestJdbc {
+    @Test
+    public void test1() {
+
+    }
+}
---
+++ mybatis/src/test/java/cn/jy/mybatis/test/TestMyBatis.java
@@ -0,0 +1,39 @@
+package cn.jy.mybatis.test;
+
+import cn.jy.mybatis.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author DengQiang.Wu
+ * @create 2019-08-24 14:53
+ */
+public class TestMyBatis {
+
+    @Test
+    public void test1() {
+        SqlSession sqlSession = null;
+        try {
+            InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+            //  创建sqlSessionFactory
+            sqlSession = factory.openSession();
+            // sqlSession
+            User user = sqlSession.selectOne("UserMapper.queryUserById", 1);
+            System.out.println(user);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+
+    }
+}
